4d802e8b4bd90dc9d5f2dc5e3ce52d4706fcdb4d
AidanStrydom <aidanracer@gmail.com>
2025-10-26 16:30:56 +0200

    added icons

--- code/scripts.js
+++ code/scripts.js
@@ -1,34 +1,32 @@
 import { podcasts } from "./data.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const modalsContainer = document.getElementById("modals");
-
-  function renderPodcastModals(list) {
-    modalsContainer.innerHTML = "";
-    list.forEach(podcast => {
-      const modal = document.createElement("div");
-      modal.classList.add("modal-card");
-      modal.innerHTML = `
-        <img src="${podcast.image}" alt="${podcast.title}">
-        <div class="modal-info">
-          <h2>${podcast.title}</h2>
-          <p>${podcast.description}</p>
-          <div class="modal-meta">
-            <strong>Seasons:</strong> ${podcast.seasons} <br>
-            <strong>Last Updated:</strong> ${new Date(podcast.updated).toLocaleDateString()}
-          </div>
-        </div>
-      `;
-
-    
-      modal.addEventListener("click", () => openPodcastModal(podcast));
-      modalsContainer.appendChild(modal);
-    });
-  }
-
   renderPodcastModals(podcasts);
 });
 
+//Render podcast modals
+function renderPodcastModals(list) {
+  const modalsContainer = document.getElementById("modals");
+  modalsContainer.innerHTML = "";
+  list.forEach(podcast => {
+    const modal = document.createElement("div");
+    modal.classList.add("modal-card");
+    modal.innerHTML = `
+      <img src="${podcast.image}" alt="${podcast.title}">
+      <div class="modal-info">
+        <h2>${podcast.title}</h2>
+        <p>${podcast.description}</p>
+        <div class="modal-meta">
+          <strong>Seasons:</strong> ${podcast.seasons} <br>
+          <strong>Last Updated:</strong> ${new Date(podcast.updated).toLocaleDateString()}
+        </div>
+      </div>
+    `;
+  
+    modal.addEventListener("click", () => openPodcastModal(podcast));
+    modalsContainer.appendChild(modal);
+  });
+}
 //Overlay modal
 
 // Modal elements
